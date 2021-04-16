140f77960822d37a09ddfc0887f382ec5e6eb36a
sungjinsama <a01081440011@gmail.com>
2021-04-16 21:22:33 +0900

    ADD StyledButton

---
+++ src/components/shared/StyledButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  border: 2px solid rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+`;
+
+export default StyledButton;
