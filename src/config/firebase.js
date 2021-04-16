1b97913c00e98e08853874259e6ae9252f06a841
sungjinsama <a01081440011@gmail.com>
2021-04-16 17:25:32 +0900

    REMOVE databaseURL

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,7 +4,6 @@ import "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PJT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
