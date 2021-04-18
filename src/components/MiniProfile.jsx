b2b880fea495fc75712bba57d06c44e0e13a97b7
sungjinsama <a01081440011@gmail.com>
2021-04-18 20:07:19 +0900

    IMPLEMENT miniprofile component

--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -9,22 +9,27 @@ const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: fit-content;
 
   // NOTE: 전체 사이즈 확인을 위한 border
   border: 2px solid black;
 `;
 
-function MiniProfile({ photo, name }) {
+function MiniProfile({ photoURL, name }) {
   return (
     <Container>
-      <Photo src={photo} />
+      <Photo
+        src={photoURL}
+        alt="profile"
+        borderRadius="100%"
+      />
       <Name name={name} />
     </Container>
   );
 }
 
 MiniProfile.propTypes = {
-  photo: PropTypes.string.isRequired,
+  photoURL: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
 };
 
