2fefe6229b22fa9fe73737a8df94d3370481d6e2
Parksoyoon <maudlinsy@gmail.com>
2021-04-22 14:42:50 +0900

    FIX fetch url in userProfile

--- src/hooks/useOtherUserProfile.js
+++ src/hooks/useOtherUserProfile.js
@@ -15,7 +15,7 @@ export default function useOtherUserProfile(id) {
     async function getUserProfile() {
       try {
         // NOTE response.data 받도록 해놨는데, 수정 필요한지 확인필요
-        const user = await fetchData("GET", `/${id}`);
+        const user = await fetchData("GET", `/user/${id}`);
 
         setUserData({
           name: user.name,
