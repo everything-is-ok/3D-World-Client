31fffc1c4cbea6c67a5c1ab0cea0e2690a1e92cd
Parksoyoon <maudlinsy@gmail.com>
2021-04-25 00:37:20 +0900

    MMAKE movingFurniture to fewer re-rendered

--- src/components/MailForm.jsx
+++ src/components/MailForm.jsx
@@ -37,7 +37,7 @@ function MailForm({ mailboxId, toggle }) {
       toggle();
     } catch (err) {
       // TODO 전송을 다시 누르게 하기
-      console.log(err.message);
+      // console.log(err.message);
     }
   }
 
---
+++ src/components/RoomFurnitures.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Floor from "./models/Floor";
+import Furniture from "./models/Furniture";
+import useFurniture from "../hooks/useFurniture";
+
+function RoomFurnitures({ socket, room, isEditMode }) {
+  const {
+    items,
+    handleSelect,
+    handleMoveItem,
+  } = useFurniture({ socket, room, isEditMode });
+
+  return (
+    <>
+      {items && items.map((item) => (
+        <Furniture
+          key={`${item._id}${Math.random(20)}`}
+          id={item._id}
+          name={item._id}
+          position={item.position}
+          isEditMode={isEditMode}
+          onClick={() => handleSelect(item._id)}
+        />
+      ))}
+      <Floor
+        width={12}
+        height={12}
+        onClick={handleMoveItem}
+      />
+    </>
+  );
+}
+
+RoomFurnitures.propTypes = {
+  socket: PropTypes.any,
+  room: PropTypes.any,
+  isEditMode: PropTypes.bool,
+};
+
+export default RoomFurnitures;
