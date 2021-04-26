37c4ffcce751c28a5a95e837b7a4c77c82ce22e6
minwooKim <vvee1253@gmail.com>
2021-04-26 14:54:03 +0900

    ADD theme constatns

---
+++ src/constants/theme.js
@@ -0,0 +1,44 @@
+const THEME = {
+  bg: {
+    title: "background color",
+    color: "gray",
+  },
+  layoutBorder1: {
+    title: "outer border color",
+    color: "black",
+  },
+  layoutBg1: {
+    title: "outer background color",
+    color: "#a9d2d9",
+  },
+  layoutBorder2: {
+    title: "inner dashed border color",
+    color: "#fff",
+  },
+  layoutBg2: {
+    title: "inner background color",
+    color: "lightgray",
+  },
+  cardBorder: {
+    title: "card border color",
+    color: "#a5a5a5",
+  },
+  cardBg: {
+    title: "card background color",
+    color: "#fff",
+  },
+  mainColor: {
+    title: "main menu background color",
+    color: "#238db3",
+  },
+  headerColor: {
+    title: "header color",
+    color: "#333",
+  },
+  textColor: {
+    title: "sub page menu color",
+    color: "#07698c",
+  },
+};
+
+export default THEME;
