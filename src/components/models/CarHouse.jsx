fb9843b18870cb20ad11ad1ca4b1bde82be802b2
minwooKim <vvee1253@gmail.com>
2021-05-02 13:24:31 +0900

    MODIFY name to email

--- src/components/models/CarHouse.jsx
+++ src/components/models/CarHouse.jsx
@@ -43,7 +43,7 @@ export default function CarHouse({
             <Texts
               scale={0.1}
               position={[-7, 10, 0]}
-              letters={user.name}
+              letters={user.email.split("@")[0]}
             />
           </Suspense>
           <Suspense fallback={null}>
